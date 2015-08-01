4e6ffb6b67fbafa790cea98277382407a313b878
Rich <aguynamedrich@gmail.com>
2015-07-31 17:23:28 -0700

    Basic UI components.  Removed Dagger library

--- app/src/main/java/com/richstern/redditdemo/MainActivity.java
+++ app/src/main/java/com/richstern/redditdemo/MainActivity.java
@@ -1,37 +1,38 @@
 package com.richstern.redditdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RxAppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    @InjectView(R.id.toolbar) Toolbar mToolbar;
+    @InjectView(R.id.swipe) SwipeRefreshLayout mSwipeRefreshLayout;
+    @InjectView(R.id.list) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Init UI components
+        ButterKnife.inject(this);
+        setSupportActionBar(mToolbar);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeColors(
+            getResources().getColor(R.color.flipagram_red),
+            getResources().getColor(R.color.accent)
+        );
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onRefresh() {
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 }
